6749e3fb49b98a709ca44b9837ce772b6834aebf
brassmacks <jadepavedpath@gmail.com>
2021-08-02 10:29:16 -0700

    'added user(s) controller and model'

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,35 @@
 class Api::UsersController < ApplicationController
+
+  def create 
+    @user = User.new(user_params)
+
+    if @user.save 
+      log_in!(@user)
+      render json: @user
+    else 
+      render json: @user.errors.full_messages
+    end
+  end
+
+  def new
+    # @user = User.new
+    # dummy user 
+    render :new
+  end
+
+  # def show
+  #   if current_user.nil?
+  #     # route to explore page
+  #   else
+  #     @user = current_user
+  #     render: json: @user.username
+  #     #isolate user/blog name to be added to current path
+  #   end
+  # end
+  protected 
+  def user_params
+    self.params.require(:user).permit(:username, :password)
+  end
+  
+  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+
   root to: 'static_pages#root'
   namespace :api, defaults: { format: :json } do
-    resources :users, only: [:create, new, :show],
+    resources :users, only: [:create, :new, :show]
     resource :session, only: [:create, :destroy, :new]
   end
+
 end
