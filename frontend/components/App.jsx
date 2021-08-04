ae960ccf980901368c5e93da7f2a5a6b79c5ab59
brassmacks <jadepavedpath@gmail.com>
2021-08-03 23:26:30 -0700

    'finished auth'

--- frontend/components/App.jsx
+++ frontend/components/App.jsx
@@ -9,13 +9,15 @@ import { Route,
 import SignupFormContainer from './session_form/SignupFormContainer';
 import LoginFormContainer from './session_form/LoginFormContainer';
 import GreetingContainer from "./greeting/greeting_container"
+import { AuthRoute, ProtectedRoute  } from '../util/route_util';
+
 const App = () => (
   <div>
     <header>
       <GreetingContainer />
     </header>
-    <Route path="/login" component={LoginFormContainer} />
-    <Route path="/signup" component={SignupFormContainer} />
+    <AuthRoute exact path="/login" component={LoginFormContainer} />
+    <AuthRoute exact path="/signup" component={SignupFormContainer} />
     
   </div>
 );
--- frontend/components/session_form/SessionForm.jsx
+++ frontend/components/session_form/SessionForm.jsx
@@ -18,7 +18,7 @@ class SessionForm extends React.Component {
   }
 
   handleSubmit(e) {
-    e.preventDefault();
+    // e.preventDefault();
     const user = Object.assign({}, this.state);
     this.props.processForm(user)
   }
