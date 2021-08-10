bcac08e2f85db738656bb77a3a42d6c682666210
brassmacks <jadepavedpath@gmail.com>
2021-08-10 13:42:29 -0700

    'debugged avatar'

--- app/views/api/posts/_post.json.jbuilder
+++ app/views/api/posts/_post.json.jbuilder
@@ -1,5 +1,6 @@
 json.extract! post, :id, :title, :user_id, :body, :content_type, :author
 json.photoUrl url_for(post.photo) if post.photo.attached?
+
 json.authorName post.author.username
 json.profileUrl url_for(post.author.profile_photo) if post.author.profile_photo.attached?
 # author association
--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -5,7 +5,7 @@ export const PostIndexItem = ({post, deletePost}) => {
     <li>
       <div>
         <h1>{post.author.username}</h1>
-        <div><img src={post.author.profileUrl} alt="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" width='100px' height="100px"></img></div>
+        <div><img src={post.profileUrl} width='100px' height="100px" alt="https://upload.wikimedia.org/wikipedia/commons/f/f9/Phoenicopterus_ruber_in_S%C3%A3o_Paulo_Zoo.jpg" ></img></div>
       <a>{post.user_id}user id</a>
       <img 
           className="post-image" 
