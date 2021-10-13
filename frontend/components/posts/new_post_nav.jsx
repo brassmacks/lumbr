a110691428cd9bf78e3364ae26b7f7faff1db6dc
brassmacks <jadepavedpath@gmail.com>
2021-10-12 20:16:42 -0700

    Route icons to prod storage

--- frontend/components/posts/new_post_nav.jsx
+++ frontend/components/posts/new_post_nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Movie from '../../../app/assets/images/movie.png'
 import PostButton from '../../components/dashboard/post_button'
-import Photo from '../../../app/assets/images/Photo.png'
-import Quote from '../../../app/assets/images/Quote.png'
-import Text from '../../../app/assets/images/Text.png'
 
 export const NewPostNav = () => {
+  
   const Lnk = 'https://lumbr-seeds.s3.us-west-1.amazonaws.com/images/Link.png'
+  const Quote = 'https://lumbr-seeds.s3.us-west-1.amazonaws.com/Quote.png'
+  const Text = 'https://lumbr-seeds.s3.us-west-1.amazonaws.com/Text.png'
+  const Photo = 'https://lumbr-seeds.s3.us-west-1.amazonaws.com/Photo.png'
+  const Movie = 'https://lumbr-seeds.s3.us-west-1.amazonaws.com/movie.png'
   let buttons = [['Text', Text], ['Photo', Photo], ['Quote', Quote], ['Link', Lnk], ['Video', Movie]]
   return (
     <div id="post-nav-panel" className="post-nav">
